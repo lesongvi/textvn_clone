be414e4d492f19d446ed90307e1223c925b3a78c
Lê Song Vĩ <rappervn88@gmail.com>
2019-10-11 13:45:31 +0700

    Update arrow.js

--- static/arrow.js
+++ static/arrow.js
@@ -1,5 +1,5 @@
     var tabLink = window.location.pathname;
-    var RQN9Api = 'YOUR RQN9 TOKEN'; //Create your RQN9 token: https://rqn9.com/developers
+    var RQN9Api = '38b1c18f052c4878b6443a0fead0bd9e'; //Create your RQN9 token: https://rqn9.com/developers
     var Webpagetitle = 'Source code'; //Your webpage title
     tokenUnderfined = 'Vui lòng kiểm tra lại thông tin giá trị token RQN9Api trong tại đường dẫn /static/arrow.js';
     somethingWrong = 'Đã có lỗi xảy ra, dữ liệu của bạn không được lưu.';
@@ -98,7 +98,7 @@
           var noteData = value;
           sessionStorage.setItem("noteData" + window.location.pathname, value);
           noteData = noteEncrypt(window.location.pathname, noteData);
-          $.get('https://api.rqn9.com/data/1.0/textvn/' + RQN9Api + tabLink + '&password=' + noteData + '&data=' + encodeURI(tabdata), function(data) {
+          $.get('https://api.rqn9.com/data/1.0/textvn/' + RQN9Api + tabLink + '&password=' + noteData + '&data=' + encodeURIComponent(tabdata), function(data) {
            var textvnApi = jQuery.parseJSON(data).response;
            if (textvnApi.status == 'correct_password') {
             alertify.success('Đã cài mật khẩu thành công!');
@@ -210,7 +210,7 @@
       var tabdata = $('.textarea').val();
       if (sessionStorage.getItem(window.location.pathname)) {
        if (sessionStorage.getItem(window.location.pathname) == noteEncrypt(window.location.pathname, sessionStorage.getItem("noteData" + window.location.pathname))) {
-        $.get('https://api.rqn9.com/data/1.0/textvn/' + RQN9Api + tabLink + '&password=' + sessionStorage.getItem(window.location.pathname) + '&data=' + encodeURI(tabdata), function(data) {
+        $.get('https://api.rqn9.com/data/1.0/textvn/' + RQN9Api + tabLink + '&password=' + sessionStorage.getItem(window.location.pathname) + '&data=' + encodeURIComponent(tabdata), function(data) {
          var textvnApi = jQuery.parseJSON(data).response;
          if (textvnApi.status == 'correct_password') {
           $('.updated').addClass('color');
@@ -218,7 +218,7 @@
         });
        }
       } else {
-       $.get('https://api.rqn9.com/data/1.0/textvn/' + RQN9Api + tabLink + '&data=' + encodeURI(tabdata), function(data) {
+       $.get('https://api.rqn9.com/data/1.0/textvn/' + RQN9Api + tabLink + '&data=' + encodeURIComponent(tabdata), function(data) {
         var textvnApi = jQuery.parseJSON(data).response;
         if (textvnApi.status == 'error' || textvnApi.message == 'password protected') {
          alertify.alert()
